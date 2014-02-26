f8f5ced7ae69c4786eb09d67642a471cbe2c3ae7
Alex Newman <posix4e@gmail.com>
2014-02-25 16:41:31 -0800

    Added a write object out step

--- src/main/java/test/ProtoStuffTimingWithOutput.java
+++ src/main/java/test/ProtoStuffTimingWithOutput.java
@@ -1,19 +1,16 @@
 package test;
 
-import com.dyuproject.protostuff.ByteString;
-
 import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.OutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.ByteBuffer;
 
-/**
- * Created by posix4e on 2/25/14.
- */
-public class ProtoStuffTiming {
+public class ProtoStuffTimingWithOutput {
 
   static ByteArrayOutputStream dataOutputStream;
-  public static void main(String args[]){
+  static ObjectOutputStream objectOutputStream;
+
+  public static void main(String args[]) throws IOException {
     int capacity = 16;
     comparisonTest(capacity);
     comparisonTest(capacity);
@@ -42,28 +39,37 @@ public class ProtoStuffTiming {
     comparisonTest(capacity);
   }
 
-  private static void comparisonTest(int capacity) {
+  private static void comparisonTest(int capacity) throws IOException {
     System.out.println("Allocating: " + capacity + " bytes ");
     ByteBuffer buffer = ByteBuffer.allocate(capacity);
     dataOutputStream = new ByteArrayOutputStream(capacity * 2);
+    objectOutputStream = new ObjectOutputStream(dataOutputStream);
+
     testProtostuffTime(buffer);
     testGoogleProtobufTime(buffer);
   }
 
-  private static void testProtostuffTime(ByteBuffer buffer) {
-    long begin,end;
+  private static void testProtostuffTime(ByteBuffer buffer) throws IOException {
+    long begin, end;
     begin = System.nanoTime();
     BigBlob1 blob1 = new BigBlob1(buffer);
+    objectOutputStream.writeObject(blob1);
+    objectOutputStream.flush();
+    dataOutputStream.flush();
     end = System.nanoTime();
     System.out.println("Protostuff:" + (end - begin));
+
   }
 
-  private static void testGoogleProtobufTime(ByteBuffer buffer) {
+  private static void testGoogleProtobufTime(ByteBuffer buffer) throws IOException {
     long begin;
     long end;
     begin = System.nanoTime();
     BigBlob.BigBlob2 blob2 = BigBlob.BigBlob2.newBuilder().setBlob(com.google.protobuf.ByteString.copyFrom(buffer.array())).build();
+    objectOutputStream.writeObject(blob2);
     end = System.nanoTime();
+    objectOutputStream.flush();
+    dataOutputStream.flush();
     System.out.println(" ProtoBuff:" + (end - begin));
   }
 }
